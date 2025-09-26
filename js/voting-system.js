e6d3caff5a2b1eaebbf173544b75c263fde2d624
Sunday <hhighfreshr5@gmail.com>
2025-09-25 22:20:28 -0500

    Update voting-system.js

--- js/voting-system.js
+++ js/voting-system.js
@@ -2,7 +2,7 @@
 
 // PASTE YOUR GOOGLE APPS SCRIPT WEB APP URL HERE
 // FIX: Updated to the LATEST DEPLOYMENT URL
-const SCRIPT_URL = "https://script.google.com/a/macros/fpsmail.org/s/AKfycbwTvyIbKfncy1EZATocookPXV2h0UhYeunXbTOS_9iFK0RfvZZiUYX4RvpFSxKzrD8gdA/exec"; 
+const SCRIPT_URL = "https://docs.google.com/spreadsheets/d/1anGNXbaR9fIlyqXR0eIplGMqoHHDVi9o15o8G0PP30U/edit?usp=sharing"; 
 const VOTE_KEY = "hasVotedDomingo";
 
 // Check if the DOM elements are available (important when deferring script)
